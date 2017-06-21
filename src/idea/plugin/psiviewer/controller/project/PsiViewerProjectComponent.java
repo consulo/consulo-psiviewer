45a3a9f958409424f7b1b8b209c3676fcd41d6c1
VISTALL <vistall.valeriy@gmail.com>
2017-06-21 08:48:53 +0300

    reformat

--- src/idea/plugin/psiviewer/controller/project/PsiViewerProjectComponent.java
+++ src/idea/plugin/psiviewer/controller/project/PsiViewerProjectComponent.java
@@ -59,290 +59,279 @@ import idea.plugin.psiviewer.controller.application.PsiViewerApplicationSettings
 import idea.plugin.psiviewer.util.Helpers;
 import idea.plugin.psiviewer.view.PsiViewerPanel;
 
-public class PsiViewerProjectComponent implements ProjectComponent, JDOMExternalizable, PsiViewerConstants {
+public class PsiViewerProjectComponent implements ProjectComponent, JDOMExternalizable, PsiViewerConstants
+{
 
-    private static final Logger LOG = Logger.getInstance("idea.plugin.psiviewer.controller.project.PsiViewerProjectComponent");
-    public boolean HIGHLIGHT = false;
-    public boolean FILTER_WHITESPACE = false;
-    public boolean SHOW_PROPERTIES = true;
-    public boolean AUTOSCROLL_TO_SOURCE = false;
-    public boolean AUTOSCROLL_FROM_SOURCE = false;
+	private static final Logger LOG = Logger.getInstance("idea.plugin.psiviewer.controller.project.PsiViewerProjectComponent");
+	public boolean HIGHLIGHT = false;
+	public boolean FILTER_WHITESPACE = false;
+	public boolean SHOW_PROPERTIES = true;
+	public boolean AUTOSCROLL_TO_SOURCE = false;
+	public boolean AUTOSCROLL_FROM_SOURCE = false;
 
-    private ComboBox myLanguagesComboBox;
-    private ItemListener myLanguagesComboBoxListener = new ItemListener() {
-        @Override
-        public void itemStateChanged(ItemEvent e)
-        {
-            if (e.getStateChange() == ItemEvent.SELECTED)
-            {
-                _viewerPanel.refreshRootElement();
-                _viewerPanel.selectElementAtCaret();
-            }
-        }
-    };
+	private ComboBox myLanguagesComboBox;
+	private ItemListener myLanguagesComboBoxListener = new ItemListener()
+	{
+		@Override
+		public void itemStateChanged(ItemEvent e)
+		{
+			if(e.getStateChange() == ItemEvent.SELECTED)
+			{
+				_viewerPanel.refreshRootElement();
+				_viewerPanel.selectElementAtCaret();
+			}
+		}
+	};
 
-    private final Project _project;
-    private EditorListener _editorListener;
-    private PsiViewerPanel _viewerPanel;
+	private final Project _project;
+	private EditorListener _editorListener;
+	private PsiViewerPanel _viewerPanel;
 
-    public PsiViewerProjectComponent(Project project)
-    {
-        _project = project;
-    }
+	public PsiViewerProjectComponent(Project project)
+	{
+		_project = project;
+	}
 
-    public void projectOpened()
-    {
-        if (PsiViewerApplicationSettings.getInstance().PLUGIN_ENABLED)
-        {
-            initToolWindow();
-        }
-    }
+	public void projectOpened()
+	{
+		if(PsiViewerApplicationSettings.getInstance().PLUGIN_ENABLED)
+		{
+			initToolWindow();
+		}
+	}
 
-    public void projectClosed()
-    {
-        unregisterToolWindow();
-    }
+	public void projectClosed()
+	{
+		unregisterToolWindow();
+	}
 
-    public void initComponent()
-    {
-    }
+	public void initComponent()
+	{
+	}
 
-    public void disposeComponent()
-    {
-    }
+	public void disposeComponent()
+	{
+	}
 
-    @NotNull
-    public String getComponentName()
-    {
-        return PLUGIN_NAME + '.' + PROJECT_COMPONENT_NAME;
-    }
+	@NotNull
+	public String getComponentName()
+	{
+		return PLUGIN_NAME + '.' + PROJECT_COMPONENT_NAME;
+	}
 
-    public void initToolWindow()
-    {
-        _viewerPanel = new PsiViewerPanel(this);
+	public void initToolWindow()
+	{
+		_viewerPanel = new PsiViewerPanel(this);
 
-        _viewerPanel.addPropertyChangeListener("ancestor", new PropertyChangeListener() {
-            public void propertyChange(PropertyChangeEvent evt)
-            {
-                handleCurrentState();
-            }
-        });
-        ActionManager actionManager = ActionManager.getInstance();
+		_viewerPanel.addPropertyChangeListener("ancestor", new PropertyChangeListener()
+		{
+			public void propertyChange(PropertyChangeEvent evt)
+			{
+				handleCurrentState();
+			}
+		});
+		ActionManager actionManager = ActionManager.getInstance();
 
-        DefaultActionGroup actionGroup = new DefaultActionGroup(ID_ACTION_GROUP, false);
-        actionGroup.add(new PropertyToggleAction("Filter Whitespace",
-                "Remove whitespace elements",
-                Helpers.getIcon(ICON_FILTER_WHITESPACE),
-                this,
-                "filterWhitespace"));
-        actionGroup.add(new PropertyToggleAction("Highlight",
-                "Highlight selected PSI element",
-                Helpers.getIcon(ICON_TOGGLE_HIGHLIGHT),
-                this,
-                "highlighted"));
-        actionGroup.add(new PropertyToggleAction("Properties",
-                "Show PSI element properties",
-                AllIcons.General.Settings,
-                this,
-                "showProperties"));
-        actionGroup.add(new PropertyToggleAction("Autoscroll to Source",
-                "Autoscroll to Source",
-                AllIcons.General.AutoscrollToSource,
-                this,
-                "autoScrollToSource"));
-        actionGroup.add(new PropertyToggleAction("Autoscroll from Source",
-                "Autoscroll from Source111",
-                AllIcons.General.AutoscrollFromSource,
-                this,
-                "autoScrollFromSource"));
+		DefaultActionGroup actionGroup = new DefaultActionGroup(ID_ACTION_GROUP, false);
+		actionGroup.add(new PropertyToggleAction("Filter Whitespace", "Remove whitespace elements", Helpers.getIcon(ICON_FILTER_WHITESPACE), this, "filterWhitespace"));
+		actionGroup.add(new PropertyToggleAction("Highlight", "Highlight selected PSI element", Helpers.getIcon(ICON_TOGGLE_HIGHLIGHT), this, "highlighted"));
+		actionGroup.add(new PropertyToggleAction("Properties", "Show PSI element properties", AllIcons.General.Settings, this, "showProperties"));
+		actionGroup.add(new PropertyToggleAction("Autoscroll to Source", "Autoscroll to Source", AllIcons.General.AutoscrollToSource, this, "autoScrollToSource"));
+		actionGroup.add(new PropertyToggleAction("Autoscroll from Source", "Autoscroll from Source111", AllIcons.General.AutoscrollFromSource, this, "autoScrollFromSource"));
 
-        ActionToolbar toolBar = actionManager.createActionToolbar(ID_ACTION_TOOLBAR, actionGroup, true);
+		ActionToolbar toolBar = actionManager.createActionToolbar(ID_ACTION_TOOLBAR, actionGroup, true);
 
-        JPanel panel = new JPanel(new HorizontalLayout(0));
-        panel.add(toolBar.getComponent());
+		JPanel panel = new JPanel(new HorizontalLayout(0));
+		panel.add(toolBar.getComponent());
 
-        myLanguagesComboBox = new ComboBox();
-        panel.add(myLanguagesComboBox);
-        updateLanguagesList(Collections.<Language>emptyList());
+		myLanguagesComboBox = new ComboBox();
+		panel.add(myLanguagesComboBox);
+		updateLanguagesList(Collections.<Language>emptyList());
 
-        _viewerPanel.add(panel, BorderLayout.NORTH);
+		_viewerPanel.add(panel, BorderLayout.NORTH);
 
-        ToolWindow toolWindow = getToolWindow();
-        toolWindow.setIcon(Helpers.getIcon(ICON_TOOL_WINDOW));
-        _viewerPanel.setToolWindow(toolWindow);
+		ToolWindow toolWindow = getToolWindow();
+		toolWindow.setIcon(Helpers.getIcon(ICON_TOOL_WINDOW));
+		_viewerPanel.setToolWindow(toolWindow);
 
-        _editorListener = new EditorListener(_viewerPanel, _project);
-    }
+		_editorListener = new EditorListener(_viewerPanel, _project);
+	}
 
-    private void handleCurrentState()
-    {
-        if (_viewerPanel == null)
-            return;
+	private void handleCurrentState()
+	{
+		if(_viewerPanel == null)
+		{
+			return;
+		}
 
-        if (_viewerPanel.isDisplayable())
-        {
-            _editorListener.start();
-            _viewerPanel.selectElementAtCaret();
-        }
-        else
-        {
-            _editorListener.stop();
-            _viewerPanel.removeHighlighting();
-        }
-    }
+		if(_viewerPanel.isDisplayable())
+		{
+			_editorListener.start();
+			_viewerPanel.selectElementAtCaret();
+		}
+		else
+		{
+			_editorListener.stop();
+			_viewerPanel.removeHighlighting();
+		}
+	}
 
-    public void unregisterToolWindow()
-    {
-        if (_viewerPanel != null)
-        {
-            _viewerPanel.removeHighlighting();
-            _viewerPanel = null;
-        }
+	public void unregisterToolWindow()
+	{
+		if(_viewerPanel != null)
+		{
+			_viewerPanel.removeHighlighting();
+			_viewerPanel = null;
+		}
 
-        if (_editorListener != null)
-        {
-            _editorListener.stop();
-            _editorListener = null;
-        }
-        if (isToolWindowRegistered())
-            ToolWindowManager.getInstance(_project).unregisterToolWindow(ID_TOOL_WINDOW);
-    }
+		if(_editorListener != null)
+		{
+			_editorListener.stop();
+			_editorListener = null;
+		}
+		if(isToolWindowRegistered())
+		{
+			ToolWindowManager.getInstance(_project).unregisterToolWindow(ID_TOOL_WINDOW);
+		}
+	}
 
-    private ToolWindow getToolWindow()
-    {
-        ToolWindowManager toolWindowManager = ToolWindowManager.getInstance(_project);
-        if (isToolWindowRegistered())
-            return toolWindowManager.getToolWindow(ID_TOOL_WINDOW);
-        else
-            return toolWindowManager.registerToolWindow(ID_TOOL_WINDOW,
-                    _viewerPanel,
-                    ToolWindowAnchor.RIGHT);
-    }
+	private ToolWindow getToolWindow()
+	{
+		ToolWindowManager toolWindowManager = ToolWindowManager.getInstance(_project);
+		if(isToolWindowRegistered())
+		{
+			return toolWindowManager.getToolWindow(ID_TOOL_WINDOW);
+		}
+		else
+		{
+			return toolWindowManager.registerToolWindow(ID_TOOL_WINDOW, _viewerPanel, ToolWindowAnchor.RIGHT);
+		}
+	}
 
-    private boolean isToolWindowRegistered()
-    {
-        return ToolWindowManager.getInstance(_project).getToolWindow(ID_TOOL_WINDOW) != null;
-    }
+	private boolean isToolWindowRegistered()
+	{
+		return ToolWindowManager.getInstance(_project).getToolWindow(ID_TOOL_WINDOW) != null;
+	}
 
-    public void readExternal(Element element) throws InvalidDataException
-    {
-        DefaultJDOMExternalizer.readExternal(this, element);
-    }
+	public void readExternal(Element element) throws InvalidDataException
+	{
+		DefaultJDOMExternalizer.readExternal(this, element);
+	}
 
-    public void writeExternal(Element element) throws WriteExternalException
-    {
-        DefaultJDOMExternalizer.writeExternal(this, element);
-    }
+	public void writeExternal(Element element) throws WriteExternalException
+	{
+		DefaultJDOMExternalizer.writeExternal(this, element);
+	}
 
-    public PsiViewerPanel getViewerPanel()
-    {
-        return _viewerPanel;
-    }
+	public PsiViewerPanel getViewerPanel()
+	{
+		return _viewerPanel;
+	}
 
-    public boolean isHighlighted()
-    {
-        return HIGHLIGHT;
-    }
+	public boolean isHighlighted()
+	{
+		return HIGHLIGHT;
+	}
 
-    public void setHighlighted(boolean highlight)
-    {
-        debug("set highlight to " + highlight);
-        HIGHLIGHT = highlight;
-        _viewerPanel.applyHighlighting();
+	public void setHighlighted(boolean highlight)
+	{
+		debug("set highlight to " + highlight);
+		HIGHLIGHT = highlight;
+		_viewerPanel.applyHighlighting();
 
-    }
+	}
 
-    public boolean isFilterWhitespace()
-    {
-        return FILTER_WHITESPACE;
-    }
+	public boolean isFilterWhitespace()
+	{
+		return FILTER_WHITESPACE;
+	}
 
-    public void setFilterWhitespace(boolean filterWhitespace)
-    {
-        FILTER_WHITESPACE = filterWhitespace;
-        getViewerPanel().applyWhitespaceFilter();
-    }
+	public void setFilterWhitespace(boolean filterWhitespace)
+	{
+		FILTER_WHITESPACE = filterWhitespace;
+		getViewerPanel().applyWhitespaceFilter();
+	}
 
-    public boolean isShowProperties()
-    {
-        return SHOW_PROPERTIES;
-    }
+	public boolean isShowProperties()
+	{
+		return SHOW_PROPERTIES;
+	}
 
-    public void setShowProperties(boolean showProperties)
-    {
-        SHOW_PROPERTIES = showProperties;
-        getViewerPanel().showProperties(showProperties);
-    }
+	public void setShowProperties(boolean showProperties)
+	{
+		SHOW_PROPERTIES = showProperties;
+		getViewerPanel().showProperties(showProperties);
+	}
 
-    public boolean isAutoScrollToSource()
-    {
-        return AUTOSCROLL_TO_SOURCE;
-    }
+	public boolean isAutoScrollToSource()
+	{
+		return AUTOSCROLL_TO_SOURCE;
+	}
 
-    public void setAutoScrollToSource(boolean isAutoScrollToSource)
-    {
-        debug("autoscrolltosource=" + isAutoScrollToSource);
-        AUTOSCROLL_TO_SOURCE = isAutoScrollToSource;
-    }
+	public void setAutoScrollToSource(boolean isAutoScrollToSource)
+	{
+		debug("autoscrolltosource=" + isAutoScrollToSource);
+		AUTOSCROLL_TO_SOURCE = isAutoScrollToSource;
+	}
 
-    public boolean isAutoScrollFromSource()
-    {
-        return AUTOSCROLL_FROM_SOURCE;
-    }
+	public boolean isAutoScrollFromSource()
+	{
+		return AUTOSCROLL_FROM_SOURCE;
+	}
 
-    public void setAutoScrollFromSource(boolean isAutoScrollFromSource)
-    {
-        debug("autoscrollfromsource=" + isAutoScrollFromSource);
-        AUTOSCROLL_FROM_SOURCE = isAutoScrollFromSource;
-    }
+	public void setAutoScrollFromSource(boolean isAutoScrollFromSource)
+	{
+		debug("autoscrollfromsource=" + isAutoScrollFromSource);
+		AUTOSCROLL_FROM_SOURCE = isAutoScrollFromSource;
+	}
 
-    public Project getProject()
-    {
-        return _project;
-    }
+	public Project getProject()
+	{
+		return _project;
+	}
 
-    public static PsiViewerProjectComponent getInstance(Project project)
-    {
-        return project.getComponent(PsiViewerProjectComponent.class);
-    }
+	public static PsiViewerProjectComponent getInstance(Project project)
+	{
+		return project.getComponent(PsiViewerProjectComponent.class);
+	}
 
-    private static void debug(String message)
-    {
-        if (LOG.isDebugEnabled())
-        {
-            LOG.debug(message);
-        }
-    }
+	private static void debug(String message)
+	{
+		if(LOG.isDebugEnabled())
+		{
+			LOG.debug(message);
+		}
+	}
 
-    @Nullable
-    public Language getSelectedLanguage()
-    {
-        return (Language) myLanguagesComboBox.getSelectedItem();
-    }
+	@Nullable
+	public Language getSelectedLanguage()
+	{
+		return (Language) myLanguagesComboBox.getSelectedItem();
+	}
 
-    public void updateLanguagesList(Collection<Language> languages)
-    {
-        Language selectedLanguage = getSelectedLanguage();
+	public void updateLanguagesList(Collection<Language> languages)
+	{
+		Language selectedLanguage = getSelectedLanguage();
 
-        myLanguagesComboBox.removeItemListener(myLanguagesComboBoxListener);
+		myLanguagesComboBox.removeItemListener(myLanguagesComboBoxListener);
 
-        //noinspection Since15
-        myLanguagesComboBox.setModel(new ListComboBoxModel<Language>(new ArrayList<Language>(languages)));
+		//noinspection Since15
+		myLanguagesComboBox.setModel(new ListComboBoxModel<Language>(new ArrayList<Language>(languages)));
 
-        if (selectedLanguage != null && languages.contains(selectedLanguage))
-        {
-            myLanguagesComboBox.setSelectedItem(selectedLanguage);
-        }
+		if(selectedLanguage != null && languages.contains(selectedLanguage))
+		{
+			myLanguagesComboBox.setSelectedItem(selectedLanguage);
+		}
 
-        if (languages.size() < 2)
-        {
-            myLanguagesComboBox.setVisible(false);
-        }
-        else
-        {
-            myLanguagesComboBox.setVisible(true);
-        }
+		if(languages.size() < 2)
+		{
+			myLanguagesComboBox.setVisible(false);
+		}
+		else
+		{
+			myLanguagesComboBox.setVisible(true);
+		}
 
-        myLanguagesComboBox.addItemListener(myLanguagesComboBoxListener);
-    }
+		myLanguagesComboBox.addItemListener(myLanguagesComboBoxListener);
+	}
 }
