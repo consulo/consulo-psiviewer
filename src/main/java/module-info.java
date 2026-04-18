7a480e893710b6e19d5ce7ebaa8156a7405b9e30
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-04-18 13:20:07 +0300

    drop ide deps

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,27 @@
  */
 open module consulo.psiviewer
 {
-	requires transitive consulo.ide.api;
+	requires consulo.application.api;
+	requires consulo.base.icon.library;
+	requires consulo.code.editor.api;
+	requires consulo.color.scheme.api;
+	requires consulo.component.api;
+	requires consulo.document.api;
+	requires consulo.file.editor.api;
+	requires consulo.language.api;
+	requires consulo.language.editor.api;
+	requires consulo.language.impl;
+	requires consulo.localize.api;
+	requires consulo.logging.api;
+	requires consulo.navigation.api;
+	requires consulo.project.api;
+	requires consulo.project.ui.api;
+	requires consulo.ui.api;
+	requires consulo.ui.ex.api;
+	requires consulo.ui.ex.awt.api;
+	requires consulo.util.collection;
+	requires consulo.util.lang;
+	requires consulo.virtual.file.system.api;
 
 	// TODO remove in future
 	requires java.desktop;
